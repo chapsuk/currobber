2b4ad289201cff292b7bf63172f6dafae4b7c0b1
mkrasilnikov <mak.krasilnikov@gmail.com>
2016-01-02 17:13:17 +0300

    license

--- src/Client/Currencylayer/Client.php
+++ src/Client/Currencylayer/Client.php
@@ -1,10 +1,23 @@
 <?php
-
+/*
+ * Copyright 2015 Maksim Krasilnikov <jo1nk1k@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 namespace Currobber\Client\Currencylayer;
 
 use Currobber\Client\AbstractClient;
 use Currobber\Result\PairRateData;
-use GuzzleHttp\Psr7\Request;
 use LogicException;
 use Psr\Http\Message\ResponseInterface;
 
